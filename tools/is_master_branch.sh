f14c5b66bd899be411f2a412fb22a1f9f16cc90f
EtienneTurc <etienne.turc@gmail.com>
2023-10-31 16:25:14 +0100

    Fix precommit

--- tools/is_master_branch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-if [ "$branch" == "master" ]
-then
-    echo "Not allowed to commit/push directly on master. Please use pull request"
-    exit 1
-fi
