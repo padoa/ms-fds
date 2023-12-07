05d70561b186a12f7c6d6cbf94fe1edbb85df6d5
Théo Casner <theo.casner@epita.fr>
2023-12-07 17:06:43 +0100

    [TECH](RC) refacto3: moved product rules to service

--- src/topics/engine/rules/__tests__/extraction-rules.test.ts
+++ src/topics/engine/rules/__tests__/extraction-rules.test.ts
@@ -6,9 +6,6 @@ import {
   applyExtractionRules,
   getDangers,
   getProducer,
-  getProduct,
-  getProductByLineOrder,
-  getProductByText,
   getSubstances,
 } from '@topics/engine/rules/extraction-rules.js';
 import type {
@@ -16,7 +13,6 @@ import type {
   IExtractedData,
   IExtractedDanger,
   IExtractedProducer,
-  IExtractedProduct,
   IExtractedSubstance,
   IFDSTree,
   IMetaData,
@@ -32,10 +28,6 @@ import {
   POSITION_PROPORTION_Y,
   PRODUCT_NAME,
   PRODUCT_IDENTIFIER_WITH_COLON,
-  PLACEHOLDER_TEXT_1,
-  PLACEHOLDER_TEXT_2,
-  PLACEHOLDER_TEXT_3,
-  TEXT_CONTENT,
   PRODUCER_NAME,
   PRODUCER_NAME_WITH_DOT,
   H_DANGER,
@@ -50,34 +42,26 @@ import {
   H_DANGER_WITH_DETAILS,
   MULTIPLE_P_DANGER_WITH_DETAILS,
   PHYSICAL_STATE_VALUE,
-  PRODUCT_IDENTIFIER,
 } from '@topics/engine/__fixtures__/fixtures.constants.js';
 import {
-  aLine,
   aLineWithCASAndCENumberIn2Texts,
   aLineWithCASNumber,
   aLineWithCENumber,
   aLineWithEUHDanger,
   aLineWithHDanger,
   aLineWithMultiplePDanger,
-  aLineWithOneText,
   aLineWithProducerIdentifierOnlyWithColon,
   aLineWithProducerEndingWithDotIn1Text,
   aLineWithProducerIn1Text,
   aLineWithProducerIn2Texts,
   aLineWithProducerNameOnly,
   aLineWithProducerWithDotIn1Text,
-  aLineWithProductIdentifierOnly,
-  aLineWithProductIn1Text,
-  aLineWithProductIn2Texts,
-  aLineWithProductNameOnly,
   aLineWithTwoDangers,
   aLineWithUndefinedText,
   aLineWithProducerIdentifierOnly,
 } from '@topics/engine/__fixtures__/line.mother.js';
 import { aSection } from '@topics/engine/__fixtures__/section.mother.js';
-import { aSubSection, aSubSectionWith3LinesContainingProductName } from '@topics/engine/__fixtures__/sub-section.mother.js';
-import { aTextWithRandomContent1, aTextWithRandomContent2, aTextWithRandomContent3 } from '@topics/engine/__fixtures__/text.mother.js';
+import { aSubSection } from '@topics/engine/__fixtures__/sub-section.mother.js';
 
 describe('ExtractionRules tests', () => {
   const iBox: IBox = { xPositionProportion: POSITION_PROPORTION_X, yPositionProportion: POSITION_PROPORTION_Y };
@@ -120,198 +104,6 @@ describe('ExtractionRules tests', () => {
         [{ input: '123-456-', expected: false }],
       ])('$input payload should return $expected', ({ input, expected }) => {
         expect(new RegExp(CENumberRegex).test(input)).toEqual(expected);
-      });
-    });
-  });
-
-  describe('Product Name rules tests', () => {
-    describe('GetProductByText tests', () => {
-      it.each<[{ message: string; fdsTree: IFDSTree; expected: IExtractedProduct | null }]>([
-        [
-          {
-            message: 'should return null when providing a fdsTree with an undefined text',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({
-                1: aSubSection().withLines([aLineWithUndefinedText().properties]).properties,
-              }).properties,
-            ).properties,
-            expected: null,
-          },
-        ],
-        [
-          {
-            message: 'should return null when providing a fdsTree with all subsections but all empty',
-            fdsTree: anEmptyFdsTreeWithAllSections().properties,
-            expected: null,
-          },
-        ],
-        [
-          {
-            message: 'should return null when providing a fdsTree with all subsections filled without product name',
-            fdsTree: aFdsTreeWithAllSectionsWithoutUsefulInfo().properties,
-            expected: null,
-          },
-        ],
-        [
-          {
-            message: 'should return product name when it is contained in 1 text',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({ 1: aSubSection().withLines([aLineWithProductIn1Text().properties]).properties }).properties,
-            ).properties,
-            expected: { name: PRODUCT_NAME, metaData },
-          },
-        ],
-        [
-          {
-            message: 'should return product name when it is contained in 2 different texts of same line',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({ 1: aSubSection().withLines([aLineWithProductIn2Texts().properties]).properties }).properties,
-            ).properties,
-            expected: { name: PRODUCT_NAME, metaData },
-          },
-        ],
-      ])('$message', ({ fdsTree, expected }) => {
-        expect(getProductByText(fdsTree)).toEqual(expected);
-      });
-    });
-
-    describe('GetProductByLineOrder tests', () => {
-      it.each<[{ message: string; fdsTree: IFDSTree; fullText: string; expected: IExtractedProduct | null }]>([
-        [
-          {
-            message: 'should return null when providing a fdsTree with an undefined text',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({
-                1: aSubSection().withLines([aLineWithUndefinedText().properties]).properties,
-              }).properties,
-            ).properties,
-            fullText: '',
-            expected: null,
-          },
-        ],
-        [
-          {
-            message: 'should return null when providing a fdsTree with all subsections but all empty',
-            fdsTree: anEmptyFdsTreeWithAllSections().properties,
-            fullText: '',
-            expected: null,
-          },
-        ],
-        [
-          {
-            message: 'should return null when providing a fdsTree with all subsections filled without product name',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({
-                1: aSubSection().withLines([
-                  aLine().withTexts([aTextWithRandomContent1().properties, aTextWithRandomContent2().properties]).properties,
-                  aLine().withTexts([aTextWithRandomContent3().properties]).properties,
-                ]).properties,
-              }).properties,
-            ).properties,
-            fullText: `${PLACEHOLDER_TEXT_1}${PLACEHOLDER_TEXT_2}${PLACEHOLDER_TEXT_3}`,
-            expected: null,
-          },
-        ],
-        [
-          {
-            message: 'should skip lines containing only product identifier',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({
-                1: aSubSection().withLines([aLineWithProductIdentifierOnly().properties]).properties,
-              }).properties,
-            ).properties,
-            fullText: PRODUCT_IDENTIFIER,
-            expected: null,
-          },
-        ],
-        [
-          {
-            message: 'should return null when product name only appears twice in fullText',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({
-                1: aSubSection().withLines([
-                  aLineWithOneText().properties,
-                  aLineWithProductNameOnly().properties,
-                  aLineWithProductNameOnly().properties,
-                ]).properties,
-              }).properties,
-            ).properties,
-            fullText: `${TEXT_CONTENT}${PRODUCT_NAME}${PRODUCT_NAME}`,
-            expected: null,
-          },
-        ],
-        [
-          {
-            message: 'should return product name when it appears three times in fullText',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({
-                1: aSubSectionWith3LinesContainingProductName().properties,
-              }).properties,
-            ).properties,
-            fullText: `${PRODUCT_NAME.repeat(3)}`,
-            expected: { name: PRODUCT_NAME, metaData },
-          },
-        ],
-        [
-          {
-            message: 'should skip first line containing product identifier and return product name when it appears three times in fullText',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({
-                1: aSubSection().withLines([
-                  aLineWithProductIdentifierOnly().properties,
-                  aLineWithProductNameOnly().properties,
-                  aLineWithProductNameOnly().properties,
-                  aLineWithProductNameOnly().properties,
-                ]).properties,
-              }).properties,
-            ).properties,
-            fullText: `${PRODUCT_IDENTIFIER}${PRODUCT_NAME.repeat(3)}`,
-            expected: { name: PRODUCT_NAME, metaData },
-          },
-        ],
-      ])('$message', ({ fdsTree, fullText, expected }) => {
-        expect(getProductByLineOrder(fdsTree, { fullText })).toEqual(expected);
-      });
-    });
-
-    describe('GetProduct tests', () => {
-      it.each<[{ message: string; fdsTree: IFDSTree; fullText: string; expected: IExtractedProduct | null }]>([
-        [
-          {
-            message: 'it should return null when providing an empty fdsTree',
-            fdsTree: anEmptyFdsTreeWithAllSections().properties,
-            fullText: '',
-            expected: null,
-          },
-        ],
-        // enters getProductByText
-        [
-          {
-            message: 'it should return product name when identifier is in one line and product in another line',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({
-                1: aSubSection().withLines([aLineWithProductIdentifierOnly().properties, aLineWithProductNameOnly().properties]).properties,
-              }).properties,
-            ).properties,
-            fullText: `${PRODUCT_IDENTIFIER_WITH_COLON}${PRODUCT_NAME}`,
-            expected: { name: PRODUCT_NAME, metaData },
-          },
-        ],
-        // enters getProductByLineOrder
-        [
-          {
-            message: 'should return product name when it appears three times or more in fullText',
-            fdsTree: aFdsTree().withSection1(
-              aSection().withSubsections({
-                1: aSubSectionWith3LinesContainingProductName().properties,
-              }).properties,
-            ).properties,
-            fullText: `${PRODUCT_NAME.repeat(3)}`,
-            expected: { name: PRODUCT_NAME, metaData },
-          },
-        ],
-      ])('$message', ({ fdsTree, fullText, expected }) => {
-        expect(getProduct(fdsTree, { fullText })).toEqual(expected);
       });
     });
   });
