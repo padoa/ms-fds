d5ffd1c348c85513cc0abcb9b8d921c0d30d55a7
Théo Casner <theo.casner@epita.fr>
2023-12-07 17:25:28 +0100

    [TECH](RC) refacto4: add readonly to regex properties

--- src/topics/engine/rules/extraction-rules/dangers-rules.service.ts
+++ src/topics/engine/rules/extraction-rules/dangers-rules.service.ts
@@ -6,9 +6,9 @@ export class DangersRulesService {
   private static readonly customHazards = ['h350i', 'h360f', 'h360d', 'h360fd', 'h360df', 'h361f', 'h361d', 'h361fd'];
 
   // TODO: add unit tests on regexes
-  public static hazardsRegex = `(${this.customHazards.join(')|(')})|(h[2-4]\\d{2})`;
-  public static precautionRegex = '(((p[1-5]\\d{2})\\+?)+)';
-  public static europeanHazardsRegex = '(euh[02]\\d{2})|(euh401)';
+  public static readonly hazardsRegex = `(${this.customHazards.join(')|(')})|(h[2-4]\\d{2})`;
+  public static readonly precautionRegex = '(((p[1-5]\\d{2})\\+?)+)';
+  public static readonly europeanHazardsRegex = '(euh[02]\\d{2})|(euh401)';
 
   public static getDangers(fdsTree: IFDSTree): IExtractedDanger[] {
     const linesToSearchIn = fdsTree[2]?.subsections?.[2]?.lines;
