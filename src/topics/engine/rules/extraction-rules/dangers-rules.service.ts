3f3a5b19f3e67bc31455582743cb0e54cf6119d5
Amcharod Yassine <yassine.a@padoa-group.com>
2023-12-08 10:29:49 +0100

    rebase

--- src/topics/engine/rules/extraction-rules/dangers-rules.service.ts
+++ src/topics/engine/rules/extraction-rules/dangers-rules.service.ts
@@ -3,23 +3,26 @@ import _ from 'lodash';
 import type { IFdsTree, IExtractedDanger } from '@topics/engine/model/fds.model.js';
 
 export class DangersRulesService {
-  private static readonly customHazards = ['h350i', 'h360f', 'h360d', 'h360fd', 'h360df', 'h361f', 'h361d', 'h361fd'];
+  private static readonly customHazards = ['h\\s*350i', 'h\\s*360f', 'h\\s*360d', 'h\\s*360fd', 'h\\s*360df', 'h\\s*361f', 'h\\s*361d', 'h\\s*361fd'];
 
   // TODO: add unit tests on regexes
-  public static readonly hazardsRegex = `(${this.customHazards.join(')|(')})|(h[2-4]\\d{2})`;
-  public static readonly precautionRegex = '(((p[1-5]\\d{2})\\+?)+)';
-  public static readonly europeanHazardsRegex = '(euh[02]\\d{2})|(euh401)';
+  public static readonly hazardsRegex = `(${this.customHazards.join(')|(')})|(h\\s*[2-4]\\d{2})`;
+  public static readonly precautionRegex = '(((p\\s*[1-5]\\d{2})\\s*\\+?\\s*)+)';
+  public static readonly europeanHazardsRegex = '(euh\\s*[02]\\d{2})|(euh\\s*401)';
 
   public static getDangers(fdsTree: IFdsTree): IExtractedDanger[] {
     const linesToSearchIn = fdsTree[2]?.subsections?.[2]?.lines;
-    const textInEachLine = _.map(linesToSearchIn, ({ texts }) => _.map(texts, 'content').join(''));
+    const infoInEachLine = _.map(linesToSearchIn, ({ texts, pageNumber, startBox, endBox }) => {
+      const textContent = texts.map((text) => text.content).join('');
+      return { text: textContent, pageNumber, startBox, endBox };
+    });
 
     // TODO: possible improvement: concat all lines then use regex once to extract dangers
-    return _(textInEachLine)
-      .map((text) => {
-        const textMatches =
-          text.replaceAll(' ', '').match(new RegExp(`${this.europeanHazardsRegex}|${this.hazardsRegex}|${this.precautionRegex}`, 'g')) || [];
-        return textMatches.map((t) => t.replaceAll('+', ' + '));
+    return _(infoInEachLine)
+      .map((lineInfo) => {
+        const { text: lineText, pageNumber, startBox, endBox } = lineInfo;
+        const textMatches = lineText.match(new RegExp(`${this.europeanHazardsRegex}|${this.hazardsRegex}|${this.precautionRegex}`, 'g')) || [];
+        return textMatches.map((text) => ({ code: _.trim(text), metaData: { pageNumber, startBox, endBox } }));
       })
       .flatMap()
       .compact()
