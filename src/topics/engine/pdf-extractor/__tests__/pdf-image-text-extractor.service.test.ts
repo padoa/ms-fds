b5906b235769635d559d391ea3504174a76da784
Théo Casner <theo.casner@epita.fr>
2023-12-08 11:18:28 +0100

    [TECH](RC) create folder and delete folder in order to parse parallel fds images

--- src/topics/engine/pdf-extractor/__tests__/pdf-image-text-extractor.service.test.ts
+++ src/topics/engine/pdf-extractor/__tests__/pdf-image-text-extractor.service.test.ts
@@ -1,4 +1,6 @@
-import { describe, expect, it } from 'vitest';
+import fsPromises from 'fs/promises';
+
+import { describe, expect, it, vi } from 'vitest';
 
 import { PdfImageTextExtractorService } from '@topics/engine/pdf-extractor/pdf-image-text-extractor.service.js';
 import { FDS_TEST_FILES_PATH } from '@src/__fixtures__/fixtures.constants.js';
@@ -6,9 +8,14 @@ import { FDS_TEST_FILES_PATH } from '@src/__fixtures__/fixtures.constants.js';
 describe('PdfImageTextExtractorService tests', () => {
   describe('getTextFromImagePdf tests', () => {
     it('should return text from image pdf', async () => {
+      const mkdirSpy = vi.spyOn(fsPromises, 'mkdir');
+      const rmSpy = vi.spyOn(fsPromises, 'rm');
+
       await expect(
         PdfImageTextExtractorService.getTextFromImagePdf(FDS_TEST_FILES_PATH.IMAGE_DEGRAISSANT, { numberOfPagesToParse: 1 }),
       ).resolves.toMatchSnapshot();
+      expect(mkdirSpy).toHaveBeenCalledOnce();
+      expect(rmSpy).toHaveBeenCalledOnce();
     });
 
     it('should return an empty list when numberOfPagesToParse is not specified', async () => {
