3e637e2c1388c193c212735f164a40cd29210c9b
EtienneTurc <etienne.turc@gmail.com>
2023-12-14 15:26:41 +0100

    Review

--- src/topics/engine/rules/extraction-rules/__tests__/__fixtures__/cas-number.builder.ts
+++ src/topics/engine/rules/extraction-rules/__tests__/__fixtures__/cas-number.builder.ts
@@ -1,13 +1,13 @@
 import { BaseBuilder } from '@padoa/meta';
 
 import { CAS_NUMBER, PAGE_NUMBER, POSITION_PROPORTION_X, POSITION_PROPORTION_Y } from '@topics/engine/__fixtures__/fixtures.constants.js';
-import type { IExtractedConcentration } from '@topics/engine/model/fds.model.js';
+import type { IExtractedCasNumber } from '@topics/engine/model/fds.model.js';
 
-export class CasNumberBuilder extends BaseBuilder<IExtractedConcentration> {
+export class CasNumberBuilder extends BaseBuilder<IExtractedCasNumber> {
   public withValue = this.withValueFor('value');
   public withMetaData = this.withValueFor('metaData');
 
-  protected getDefaultValues(): IExtractedConcentration {
+  protected getDefaultValues(): IExtractedCasNumber {
     return {
       value: CAS_NUMBER,
       metaData: {
--- src/topics/engine/rules/extraction-rules/__tests__/__fixtures__/ce-number.builder.ts
+++ src/topics/engine/rules/extraction-rules/__tests__/__fixtures__/ce-number.builder.ts
@@ -1,13 +1,13 @@
 import { BaseBuilder } from '@padoa/meta';
 
 import { CE_NUMBER, PAGE_NUMBER, POSITION_PROPORTION_X, POSITION_PROPORTION_Y } from '@topics/engine/__fixtures__/fixtures.constants.js';
-import type { IExtractedConcentration } from '@topics/engine/model/fds.model.js';
+import type { IExtractedCeNumber } from '@topics/engine/model/fds.model.js';
 
-export class CeNumberBuilder extends BaseBuilder<IExtractedConcentration> {
+export class CeNumberBuilder extends BaseBuilder<IExtractedCeNumber> {
   public withValue = this.withValueFor('value');
   public withMetaData = this.withValueFor('metaData');
 
-  protected getDefaultValues(): IExtractedConcentration {
+  protected getDefaultValues(): IExtractedCeNumber {
     return {
       value: CE_NUMBER,
       metaData: {
